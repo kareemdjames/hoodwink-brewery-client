4c1786ebbf2a9b5aa69952fc5a02f107f4060e61
Kareem James <kareemdjames@gmail.com>
2020-07-10 18:14:50 -0400

    created rest template, set apihost property. closes #2

---
+++ src/test/java/com/hoodwink/hoodwinkbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,26 @@
+package com.hoodwink.hoodwinkbreweryclient.web.client;
+
+import com.hoodwink.hoodwinkbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDto);
+
+    }
+}
